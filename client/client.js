8b4d3ace74c56b00636327b87c09ac5cfb0fffa7
Alva Bengtsson <alvben0721@edu.halmstad.se>
2026-01-27 12:54:37 +0100

    20min reactExpress vid

--- client/client.js
+++ client/client.js
@@ -17,13 +17,10 @@ function App() {
 };
 
 function Element(){
-
     const [element, setElement] = React.useState([]);
-
     React.useEffect(()=>{
-
+        getElement();
     }, [])
-
 
     async function getElement(){
         const res = await fetch("/element");
@@ -31,10 +28,22 @@ function Element(){
         setElement(data);
     };
 
+    async function delE(id){
+        alert(id)
+
+    }
+
     return(
         <div>
-            <h3>Element från servern</h3>
-            {JSON.stringify(element)}
+            <h2>Element från servern</h2>
+            {element.map(e=>(
+                <div class="element" key={e.id}>
+                    <h3>{e.brand}</h3> 
+                    <h4>{e.id}</h4>                   
+                    <h5><i>{e.price}</i></h5> 
+                    <button onClick={()=>(delE(e.id))}>Delete</button>                   
+                </div>
+            ))}
         </div>
     );
 };
